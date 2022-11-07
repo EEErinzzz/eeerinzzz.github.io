4ffb7b62fd4a85bfd89e9b62271da7df135596a9
EEErinzzz <116625261+EEErinzzz@users.noreply.github.com>
2022-11-08 00:01:02 +0800

    Update index.js

--- HB/js/index.js
+++ HB/js/index.js
@@ -1,31 +1,3 @@
-Skip to content
-Search or jump to…
-Pulls
-Issues
-Marketplace
-Explore
- 
-@EEErinzzz 
-EEErinzzz
-/
-HB-HT
-Private
-Code
-Issues
-Pull requests
-Actions
-Projects
-Security
-Insights
-More
-HB-HT/js/index.js /
-@EEErinzzz
-EEErinzzz Create index.js
-Latest commit 1c75c90 23 minutes ago
- History
- 1 contributor
-29 lines (26 sloc)  711 Bytes
-
 (function() {
   function $(id) {
     return document.getElementById(id);
@@ -55,18 +27,3 @@ Latest commit 1c75c90 23 minutes ago
   });
 
 }());
-Footer
-© 2022 GitHub, Inc.
-Footer navigation
-Terms
-Privacy
-Security
-Status
-Docs
-Contact GitHub
-Pricing
-API
-Training
-Blog
-About
-HB-HT/index.js at main · EEErinzzz/HB-HT
